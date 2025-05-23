8dfef5c6dcf91393c9a13cfd6718e26f47bb8e67
JimSykora <14374121+JimSycurity@users.noreply.github.com>
2025-05-23 13:40:17 -0500

    Updates

--- Add-BadSuccessorOUDenyACEs.ps1
+++ Add-BadSuccessorOUDenyACEs.ps1
@@ -3,6 +3,8 @@ function Add-BadSuccessorOUDenyACEs {
     .SYNOPSIS
         Applies targetted deny ACEs to OUs such that the BadSuccessor dMSA abuse will not be available
 
+        To cover all OUs in the domain pipe Get-ADOrganizationalUnit -Filter * | Add-BadSuccessorOUDenyACEs
+
     .DESCRIPTION
         Optionally takes an Organizational Unit (OU) distinguishedName path for targetted mitigation, or with
         the optional -All parameter can apply the remediations to All OUs in the domain.
@@ -19,67 +21,50 @@ function Add-BadSuccessorOUDenyACEs {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position=0, ValueFromPipeline)]
-        [string[]]$TargetOUs,
-        [switch]$All
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string]$DistinguishedName
     )
 
     begin {
-        if ($All) {
-            $currentDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-            $domain = [ADSI]"LDAP://$currentDomain"
-            $searcher = [System.DirectoryServices.DirectorySearcher]::new($domain)
-            $searcher.SearchScope = [System.DirectoryServices.SearchScope]::Subtree
-            $searcher.SecurityMasks = 7
-            $searcher.Filter = '(objectclass=organizationalUnit)'
-            $TargetOUs = ($searcher.FindAll()).Properties.distinguishedname
-        }
         $dMSA = [GUID]'0feb936f-47b3-49f2-9386-1dedc2c23765'
         $msDSManagedAccountPrecededByLink = [GUID]'a0945b2b-57a2-43bd-b327-4d112a4e8bd1'
-        $protectedOUs = @()
     }
 
     process {
-        foreach ($DN in $TargetOUs) {
-            if ([ADSI]::Exists("LDAP://$DN")) {
-                Write-Verbose "OU Exists: $DN"
-                $OU = [ADSI]"LDAP://$DN"
-            } else {
-                Write-Warning "Invalid OU: $DN"
-                continue
-            }
-
-            $OU.PsBase.Options.SecurityMasks = 'Dacl'
-
-            # Deny Authenticated Users Create Child dMSA
-            $GroupSID =  [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
-            $Identity = [System.Security.Principal.IdentityReference] $GroupSID
-            $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "CreateChild"
-            $Type = [System.Security.AccessControl.AccessControlType] "Deny"
-            $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $dMSA)
-            $OU.PsBase.ObjectSecurity.AddAccessRule($Rule)
-            # Implement OwnerRights on Child dMSA
-            $GroupSID =  [System.Security.Principal.SecurityIdentifier]::new("S-1-3-4")
-            $Identity = [System.Security.Principal.IdentityReference] $GroupSID
-            $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "ListChildren"
-            $Type = [System.Security.AccessControl.AccessControlType] "Allow"
-            $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
-            $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $InheritanceType, $dMSA)
-            $OU.PsBase.ObjectSecurity.AddAccessRule($Rule)
-            # Deny Authenticated Users WriteProperty msDS-ManagedAccountPrecededByLink on child dMSA
-            $GroupSID =  [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
-            $Identity = [System.Security.Principal.IdentityReference] $GroupSID
-            $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "WriteProperty"
-            $Type = [System.Security.AccessControl.AccessControlType] "Deny"
-            $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "Descendents"
-            $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $msDSManagedAccountPrecededByLink, $InheritanceType, $dMSA)
-            $OU.PsBase.ObjectSecurity.AddAccessRule($Rule)
-            $OU.PsBase.CommitChanges()
+        if ([ADSI]::Exists("LDAP://$DistinguishedName")) {
+            Write-Verbose "OU Exists: $DistinguishedName"
+            $OU = [ADSI]"LDAP://$DistinguishedName"
+        }
+        else {
+            Write-Warning "Invalid OU: $DistinguishedName"
+            continue
         }
 
-    }
+        $OU.PsBase.Options.SecurityMasks = 'Dacl'
 
-    end {
-
+        # Add Deny Authenticated Users Create Child dMSA ACE
+        $GroupSID = [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
+        $Identity = [System.Security.Principal.IdentityReference] $GroupSID
+        $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "CreateChild"
+        $Type = [System.Security.AccessControl.AccessControlType] "Deny"
+        $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $dMSA)
+        $OU.PsBase.ObjectSecurity.AddAccessRule($Rule)
+        # Add Allow OwnerRights ListContent on Child dMSA ACE
+        $GroupSID = [System.Security.Principal.SecurityIdentifier]::new("S-1-3-4")
+        $Identity = [System.Security.Principal.IdentityReference] $GroupSID
+        $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "ListChildren"
+        $Type = [System.Security.AccessControl.AccessControlType] "Allow"
+        $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
+        $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $InheritanceType, $dMSA)
+        $OU.PsBase.ObjectSecurity.AddAccessRule($Rule)
+        # Add Deny Authenticated Users WriteProperty msDS-ManagedAccountPrecededByLink on child dMSA ACE
+        $GroupSID = [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
+        $Identity = [System.Security.Principal.IdentityReference] $GroupSID
+        $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "WriteProperty"
+        $Type = [System.Security.AccessControl.AccessControlType] "Deny"
+        $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "Descendents"
+        $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $msDSManagedAccountPrecededByLink, $InheritanceType, $dMSA)
+        $OU.PsBase.ObjectSecurity.AddAccessRule($Rule)
+        $OU.PsBase.CommitChanges()
     }
 }
--- Remove-BadSuccessorOUDenyACEs.ps1
+++ Remove-BadSuccessorOUDenyACEs.ps1
@@ -3,6 +3,8 @@ function Remove-BadSuccessorOUDenyACEs {
     .SYNOPSIS
         Removes targetted deny ACEs to OUs such that were created with Add-BadSuccessorOUDenyACEs
 
+        To cover all OUs in the domain pipe Get-ADOrganizationalUnit -Filter * | Remove-BadSuccessorOUDenyACEs
+
     .DESCRIPTION
         Optionally takes an Organizational Unit (OU) distinguishedName path for targetted mitigation, or with
         the optional -All parameter can apply the remediations to All OUs in the domain.
@@ -13,67 +15,51 @@ function Remove-BadSuccessorOUDenyACEs {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position=0, ValueFromPipeline)]
-        [string[]]$TargetOUs,
-        [switch]$All
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$DistinguishedName
     )
 
     begin {
-        if ($All) {
-            $currentDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-            $domain = [ADSI]"LDAP://$currentDomain"
-            $searcher = [System.DirectoryServices.DirectorySearcher]::new($domain)
-            $searcher.SearchScope = [System.DirectoryServices.SearchScope]::Subtree
-            $searcher.SecurityMasks = 7
-            $searcher.Filter = '(objectclass=organizationalUnit)'
-            $TargetOUs = ($searcher.FindAll()).Properties.distinguishedname
-        }
         $dMSA = [GUID]'0feb936f-47b3-49f2-9386-1dedc2c23765'
         $msDSManagedAccountPrecededByLink = [GUID]'a0945b2b-57a2-43bd-b327-4d112a4e8bd1'
-        $protectedOUs = @()
     }
 
     process {
-        foreach ($DN in $TargetOUs) {
-            if ([ADSI]::Exists("LDAP://$DN")) {
-                Write-Verbose "OU Exists: $DN"
-                $OU = [ADSI]"LDAP://$DN"
-            } else {
-                Write-Warning "Invalid OU: $DN"
-                continue
-            }
-
-            $OU.PsBase.Options.SecurityMasks = 'Dacl'
-
-            # Deny Authenticated Users Create Child dMSA
-            $GroupSID =  [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
-            $Identity = [System.Security.Principal.IdentityReference] $GroupSID
-            $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "CreateChild"
-            $Type = [System.Security.AccessControl.AccessControlType] "Deny"
-            $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $dMSA)
-            $OU.PsBase.ObjectSecurity.RemoveAccessRuleSpecific($Rule)
-            # Implement OwnerRights on Child dMSA
-            $GroupSID =  [System.Security.Principal.SecurityIdentifier]::new("S-1-3-4")
-            $Identity = [System.Security.Principal.IdentityReference] $GroupSID
-            $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "ListChildren"
-            $Type = [System.Security.AccessControl.AccessControlType] "Allow"
-            $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
-            $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $InheritanceType, $dMSA)
-            $OU.PsBase.ObjectSecurity.RemoveAccessRuleSpecific($Rule)
-            # Deny Authenticated Users WriteProperty msDS-ManagedAccountPrecededByLink on child dMSA
-            $GroupSID =  [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
-            $Identity = [System.Security.Principal.IdentityReference] $GroupSID
-            $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "WriteProperty"
-            $Type = [System.Security.AccessControl.AccessControlType] "Deny"
-            $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "Descendents"
-            $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $msDSManagedAccountPrecededByLink, $InheritanceType, $dMSA)
-            $OU.PsBase.ObjectSecurity.RemoveAccessRuleSpecific($Rule)
-            $OU.PsBase.CommitChanges()
+        if ([ADSI]::Exists("LDAP://$DistinguishedName")) {
+            Write-Verbose "OU Exists: $DistinguishedName"
+            $OU = [ADSI]"LDAP://$DistinguishedName"
+        }
+        else {
+            Write-Warning "Invalid OU: $DistinguishedName"
+            continue
         }
 
-    }
+        $OU.PsBase.Options.SecurityMasks = 'Dacl'
 
-    end {
-
+        # Remove Deny AuthenticatedUsers CreateChild dMSA ACE
+        $GroupSID = [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
+        $Identity = [System.Security.Principal.IdentityReference] $GroupSID
+        $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "CreateChild"
+        $Type = [System.Security.AccessControl.AccessControlType] "Deny"
+        $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $dMSA)
+        $OU.PsBase.ObjectSecurity.RemoveAccessRuleSpecific($Rule)
+        <# Uncomment to remove Allow OwnerRights ListContent on Child dMSA ACE
+        $GroupSID = [System.Security.Principal.SecurityIdentifier]::new("S-1-3-4")
+        $Identity = [System.Security.Principal.IdentityReference] $GroupSID
+        $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "ListChildren"
+        $Type = [System.Security.AccessControl.AccessControlType] "Allow"
+        $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
+        $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $InheritanceType, $dMSA)
+        $OU.PsBase.ObjectSecurity.RemoveAccessRuleSpecific($Rule)
+        #>
+        # Remove Deny AuthenticatedUsers WriteProperty msDS-ManagedAccountPrecededByLink on child dMSA ACE
+        $GroupSID = [System.Security.Principal.SecurityIdentifier]::new("S-1-5-11")
+        $Identity = [System.Security.Principal.IdentityReference] $GroupSID
+        $ADRight = [System.DirectoryServices.ActiveDirectoryRights] "WriteProperty"
+        $Type = [System.Security.AccessControl.AccessControlType] "Deny"
+        $InheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "Descendents"
+        $Rule = [System.DirectoryServices.ActiveDirectoryAccessRule]::new($Identity, $ADRight, $Type, $msDSManagedAccountPrecededByLink, $InheritanceType, $dMSA)
+        $OU.PsBase.ObjectSecurity.RemoveAccessRuleSpecific($Rule)
+        $OU.PsBase.CommitChanges()
     }
 }
