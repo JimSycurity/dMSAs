10bb5f9c3ccadcfd0fe7a4098d363d8d749d1365
JimSykora <14374121+JimSycurity@users.noreply.github.com>
2025-05-27 17:38:37 -0500

    Added Get-dMSASchema.ps1 to determine if the schema of the AD Forest allows dMSA to be created under any other object classes than OU or container.

---
+++ Experiments/Get-dMSASchema.ps1
@@ -0,0 +1,26 @@
+# Check the AD Forest Schema to determine if any objectClasses other than organizationalUnit or container allow msDS-DelegatedManagedServiceAccount child objects.
+# For instance if CVE-2021-34470 is still in play, it could be possible to create a dMSA nested under a computer object.
+
+Import-Module ActiveDirectory
+$defaultSystemPossSuperiors = @('container', 'organizationalUnit')
+$defaultState = $true
+$rootDSE = Get-ADRootDSE
+$schemaPath = $rootDSE.schemanamingContext
+$dMSAObjectClass = Get-AdObject -Filter 'lDAPDisplayName -eq "msDS-DelegatedManagedServiceAccount"' -SearchBase $schemaPath -Properties *
+foreach ($class in $dMSAObjectClass.systemPossSuperiors ) {
+    if ($class -notin $defaultSystemPossSuperiors) {
+        Write-Host "Found non-default objectClass: $class in dMSA systemPossSuperiors."
+        $defaultState = $false
+    }
+}
+if ($dMSAObjectClass.possSuperiors -ne '') {
+    Write-Houst "Non-default objectClass(es) in possSuperiors: $($dMSAObjectClass.possSuperiors)"
+    $defaultState = $false
+}
+
+if ($defaultState) {
+    Write-Host 'The AD Forest has a default AD Schema of only allowing dMSAs to be created under OUs and containers.' -ForegroundColor DarkGreen
+}
+else {
+    Write-Host 'The AD Forest allows dMSA objects to be created as children of more than the default OU and container!' -ForegroundColor DarkRed
+}
